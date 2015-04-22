74f10f5d17de00402c8dfc89529db8a569f85743
Xin Wu <fiona330@126.com>
2015-04-22 15:33:17 -0400

    Update DetailViewController.m

--- WXWeibo/WXWeibo/DetailViewController.m
+++ WXWeibo/WXWeibo/DetailViewController.m
@@ -1,11 +1,3 @@
-//
-//  DetailViewController.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-3.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "DetailViewController.h"
 #import "UIImageView+WebCache.h"
 #import "WeiboModel.h"

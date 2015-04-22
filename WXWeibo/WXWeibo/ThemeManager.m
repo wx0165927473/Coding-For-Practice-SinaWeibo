ad7e4d42055e6228d11503698113aef57e91d6e9
Xin Wu <fiona330@126.com>
2015-04-22 15:49:01 -0400

    Update ThemeManager.m

--- WXWeibo/WXWeibo/ThemeManager.m
+++ WXWeibo/WXWeibo/ThemeManager.m
@@ -1,11 +1,3 @@
-//
-//  ThemeManager.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-22.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "ThemeManager.h"
 
 static ThemeManager *sigleton = nil;

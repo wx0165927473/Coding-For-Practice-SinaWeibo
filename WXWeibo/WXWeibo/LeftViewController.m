345486ea9dda8fe2f28907078caef1079793333e
Xin Wu <fiona330@126.com>
2015-04-22 15:38:28 -0400

    Update LeftViewController.m

--- WXWeibo/WXWeibo/LeftViewController.m
+++ WXWeibo/WXWeibo/LeftViewController.m
@@ -1,11 +1,3 @@
-//
-//  LeftViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "LeftViewController.h"
 
 @interface LeftViewController ()

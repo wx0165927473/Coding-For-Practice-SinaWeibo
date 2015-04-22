85155937bce60585bb2b86b2f7779c4b523ba079
Xin Wu <fiona330@126.com>
2015-04-22 15:36:32 -0400

    Update HomeViewController.m

--- WXWeibo/WXWeibo/HomeViewController.m
+++ WXWeibo/WXWeibo/HomeViewController.m
@@ -1,11 +1,3 @@
-//
-//  HomeViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "HomeViewController.h"
 #import "WeiboModel.h"
 #import "UIFactory.h"

82c383cda7c006af6ffc7727b335bf87e117eb3d
Xin Wu <fiona330@126.com>
2015-04-22 15:29:01 -0400

    Update BaseNavigationController.m

--- WXWeibo/WXWeibo/BaseNavigationController.m
+++ WXWeibo/WXWeibo/BaseNavigationController.m
@@ -1,11 +1,3 @@
-//
-//  BaseNavigationController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseNavigationController.h"
 #import "ThemeManager.h"
 #import "AppDelegate.h"

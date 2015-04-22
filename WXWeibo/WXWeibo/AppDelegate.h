d1da1a3119075f7ca1d5d4399fde5d430c419283
Xin Wu <fiona330@126.com>
2015-04-22 00:54:19 -0400

    Update AppDelegate.h

--- WXWeibo/WXWeibo/AppDelegate.h
+++ WXWeibo/WXWeibo/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "DDMenuController.h"

6d5c43bb6f4f1e321d2aea4d63ffc0ae164ed781
Xin Wu <fiona330@126.com>
2015-04-22 15:41:07 -0400

    Update NearByViewController.h

--- WXWeibo/WXWeibo/NearByViewController.h
+++ WXWeibo/WXWeibo/NearByViewController.h
@@ -1,11 +1,3 @@
-//
-//  NearByViewController.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-12.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 #import <CoreLocation/CoreLocation.h>
 

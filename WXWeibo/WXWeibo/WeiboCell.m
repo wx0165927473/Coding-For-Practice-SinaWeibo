c6e3ca6996b2e5e770328ce5b75955bd79408c99
Xin Wu <fiona330@126.com>
2015-04-22 15:58:46 -0400

    Update WeiboCell.m

--- WXWeibo/WXWeibo/WeiboCell.m
+++ WXWeibo/WXWeibo/WeiboCell.m
@@ -1,11 +1,3 @@
-//
-//  WeiboCell.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-23.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "WeiboCell.h"
 #import <QuartzCore/QuartzCore.h>
 #import "WeiboView.h"

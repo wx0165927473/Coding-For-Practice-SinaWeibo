b627dcdc68a4ef6e947443ed3afa28d594d9c96d
Xin Wu <fiona330@126.com>
2015-04-22 15:53:23 -0400

    Update UIView+Additions.h

--- WXWeibo/WXWeibo/UIView+Additions.h
+++ WXWeibo/WXWeibo/UIView+Additions.h
@@ -1,11 +1,3 @@
-//
-//  UIView+Additions.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-9.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface UIView (Additions)

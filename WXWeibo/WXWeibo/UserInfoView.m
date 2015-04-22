f3409f21be72e39b8389c9cfab65e23cd5bdacb7
Xin Wu <fiona330@126.com>
2015-04-22 15:55:02 -0400

    Update UserInfoView.m

--- WXWeibo/WXWeibo/UserInfoView.m
+++ WXWeibo/WXWeibo/UserInfoView.m
@@ -1,11 +1,3 @@
-//
-//  UserInfoView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-9.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "UserInfoView.h"
 #import "UIImageView+WebCache.h"
 #import "FriendShipsViewController.h"

be7cdc4f46fe0ebc6891fb9b9f2203c6cbbd8249
Xin Wu <fiona330@126.com>
2015-04-22 15:44:47 -0400

    Update RightViewController.m

--- WXWeibo/WXWeibo/RightViewController.m
+++ WXWeibo/WXWeibo/RightViewController.m
@@ -1,11 +1,3 @@
-//
-//  RightViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "RightViewController.h"
 #import "SendViewController.h"
 #import "BaseNavigationController.h"

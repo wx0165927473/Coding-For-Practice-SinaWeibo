4c7eb6befa62128e580f2d594da415baca4137b0
Xin Wu <fiona330@126.com>
2015-04-22 15:58:09 -0400

    Update WebViewController.h

--- WXWeibo/WXWeibo/WebViewController.h
+++ WXWeibo/WXWeibo/WebViewController.h
@@ -1,11 +1,3 @@
-//
-//  WebViewController.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-10.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 
 @interface WebViewController : BaseViewController<UIWebViewDelegate> {

c72f2f7c2bdf453a7e54c83f228e22be5cff4eb8
Xin Wu <fiona330@126.com>
2015-04-22 15:29:13 -0400

    Update BaseTableView.m

--- WXWeibo/WXWeibo/BaseTableView.m
+++ WXWeibo/WXWeibo/BaseTableView.m
@@ -1,11 +1,3 @@
-//
-//  BaseTableView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-1.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseTableView.h"
 
 @implementation BaseTableView

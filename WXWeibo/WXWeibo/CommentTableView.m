13e6b4089c1116dbd62fed2736c2c74ee47dca6f
Xin Wu <fiona330@126.com>
2015-04-22 15:32:42 -0400

    Update CommentTableView.m

--- WXWeibo/WXWeibo/CommentTableView.m
+++ WXWeibo/WXWeibo/CommentTableView.m
@@ -1,11 +1,3 @@
-//
-//  CommentTableView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-5.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "CommentTableView.h"
 #import "CommentCell.h"
 #import "CommentModel.h"

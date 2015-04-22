b64f033349e4ccd2fdca988745d872909b15b411
Xin Wu <fiona330@126.com>
2015-04-22 15:55:49 -0400

    Update UserViewController.m

--- WXWeibo/WXWeibo/UserViewController.m
+++ WXWeibo/WXWeibo/UserViewController.m
@@ -1,11 +1,3 @@
-//
-//  UserViewController.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-9.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "UserViewController.h"
 #import "UIFactory.h"
 #import "UserModel.h"

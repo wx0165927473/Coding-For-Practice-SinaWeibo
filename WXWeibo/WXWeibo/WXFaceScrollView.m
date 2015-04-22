ff8576be0a13a244da11bd49ef6652d6d1d7a6fa
Xin Wu <fiona330@126.com>
2015-04-22 15:56:56 -0400

    Update WXFaceScrollView.m

--- WXWeibo/WXWeibo/WXFaceScrollView.m
+++ WXWeibo/WXWeibo/WXFaceScrollView.m
@@ -1,11 +1,3 @@
-//
-//  WXFaceScrollView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-15.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "WXFaceScrollView.h"
 
 @implementation WXFaceScrollView

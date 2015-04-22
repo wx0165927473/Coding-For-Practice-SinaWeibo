615dea90f6bf837070409199b7f1b371097929e9
Xin Wu <fiona330@126.com>
2015-04-22 01:02:39 -0400

    Update AtUserCell.h

--- WXWeibo/WXWeibo/AtUserCell.h
+++ WXWeibo/WXWeibo/AtUserCell.h
@@ -1,11 +1,3 @@
-//
-//  AtUserCell.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-19.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "UserModel.h"
 

6b44ee7baf6a30371b145b8179cfe19d11bda885
Xin Wu <fiona330@126.com>
2015-04-22 15:55:56 -0400

    Update WXBaseModel.h

--- WXWeibo/WXWeibo/WXBaseModel.h
+++ WXWeibo/WXWeibo/WXBaseModel.h
@@ -1,12 +1,3 @@
-//
-//  WXBaseModel.h
-//  MTWeibo
-//  所有对象实体的基类
-
-//  Created by wei.chen on 11-9-22.
-//  Copyright 2011年 www.iphonetrain.com 无限互联ios开发培训中心 All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface WXBaseModel : NSObject <NSCoding>{

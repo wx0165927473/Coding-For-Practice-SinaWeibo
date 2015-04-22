a7f237973e570df02917ac5afa76169d666bb284
Xin Wu <fiona330@126.com>
2015-04-22 15:32:03 -0400

    Update CommentModel.h

--- WXWeibo/WXWeibo/CommentModel.h
+++ WXWeibo/WXWeibo/CommentModel.h
@@ -1,11 +1,3 @@
-//
-//  CommentModel.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-5.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "WXBaseModel.h"
 #import "WeiboModel.h"
 #import "UserModel.h"

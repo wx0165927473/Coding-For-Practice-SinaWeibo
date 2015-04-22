7ebcdecb4ac3b7aeb66eb5f4cb7221f1a388439a
Xin Wu <fiona330@126.com>
2015-04-22 15:44:08 -0400

    Update RightViewController.h

--- WXWeibo/WXWeibo/RightViewController.h
+++ WXWeibo/WXWeibo/RightViewController.h
@@ -1,11 +1,3 @@
-//
-//  RightViewController.h
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 
 @interface RightViewController : BaseViewController <UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>

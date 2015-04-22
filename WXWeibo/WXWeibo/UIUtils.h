0e6b22c39b9faaf6271d8b3458b4edb9c7c5cc37
Xin Wu <fiona330@126.com>
2015-04-22 15:52:12 -0400

    Update UIUtils.h

--- WXWeibo/WXWeibo/UIUtils.h
+++ WXWeibo/WXWeibo/UIUtils.h
@@ -1,11 +1,3 @@
-//
-//  UIUtils.h
-//  WXTime
-//
-//  Created by wei.chen on 12-7-22.
-//  Copyright (c) 2012年 www.iphonetrain.com 无限互联ios开发培训中心 All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface UIUtils : NSObject

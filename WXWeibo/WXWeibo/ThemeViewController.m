7404ae7a9780a929a218bf3712822b68e3acea61
Xin Wu <fiona330@126.com>
2015-04-22 15:49:17 -0400

    Update ThemeViewController.m

--- WXWeibo/WXWeibo/ThemeViewController.m
+++ WXWeibo/WXWeibo/ThemeViewController.m
@@ -1,11 +1,3 @@
-//
-//  ThemeViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-22.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "ThemeViewController.h"
 #import "ThemeManager.h"
 #import "UIFactory.h"

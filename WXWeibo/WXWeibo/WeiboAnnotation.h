2344c414e514f3d9a4b17aa8673d053d206496f5
Xin Wu <fiona330@126.com>
2015-04-22 15:58:21 -0400

    Update WeiboAnnotation.h

--- WXWeibo/WXWeibo/WeiboAnnotation.h
+++ WXWeibo/WXWeibo/WeiboAnnotation.h
@@ -1,11 +1,3 @@
-//
-//  WeiboAnnotation.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-16.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <MapKit/MapKit.h>
 #import "WeiboModel.h"

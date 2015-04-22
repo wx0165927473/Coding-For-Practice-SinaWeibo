19f4545bd15577029c6bbe5566bc97bf0d787ac0
Xin Wu <fiona330@126.com>
2015-04-22 15:53:45 -0400

    Update UserAnnotation.h

--- WXWeibo/WXWeibo/UserAnnotation.h
+++ WXWeibo/WXWeibo/UserAnnotation.h
@@ -1,11 +1,3 @@
-//
-//  UserAnnotation.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-16.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <MapKit/MapKit.h>
 #import "UserModel.h"

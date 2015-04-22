1562783c177f89c6b9170cb381fb932e1068aa73
Xin Wu <fiona330@126.com>
2015-04-22 15:59:07 -0400

    Update WeiboTableView.m

--- WXWeibo/WXWeibo/WeiboTableView.m
+++ WXWeibo/WXWeibo/WeiboTableView.m
@@ -1,11 +1,3 @@
-//
-//  WeiboTableView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-1.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "WeiboTableView.h"
 #import "WeiboCell.h"
 #import "WeiboModel.h"

6544b6f9f31cfcc8b130425105bce8239e7bc123
Xin Wu <fiona330@126.com>
2015-04-22 15:51:45 -0400

    Update UIFactory.m

--- WXWeibo/WXWeibo/UIFactory.m
+++ WXWeibo/WXWeibo/UIFactory.m
@@ -1,11 +1,3 @@
-//
-//  UIFactory.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-22.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "UIFactory.h"
 
 @implementation UIFactory

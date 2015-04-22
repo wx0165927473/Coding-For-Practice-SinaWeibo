29f3960547806ca327f902571bbbaa5fc3da442c
Xin Wu <fiona330@126.com>
2015-04-22 15:33:35 -0400

    Update DiscoverViewController.m

--- WXWeibo/WXWeibo/DiscoverViewController.m
+++ WXWeibo/WXWeibo/DiscoverViewController.m
@@ -1,11 +1,3 @@
-//
-//  DiscoverViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "DiscoverViewController.h"
 #import "NearWeiboMapViewController.h"
 #import "NearUserViewController.h"

861b02f0c13fb2490ddbecd499a1d337e7363baf
Xin Wu <fiona330@126.com>
2015-04-22 15:38:53 -0400

    Update MessageViewController.h

--- WXWeibo/WXWeibo/MessageViewController.h
+++ WXWeibo/WXWeibo/MessageViewController.h
@@ -1,12 +1,3 @@
-//
-//  MessageViewController.h
-//  WXWeibo
-//  消息首页控制器
-
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 #import "WeiboTableView.h"
 

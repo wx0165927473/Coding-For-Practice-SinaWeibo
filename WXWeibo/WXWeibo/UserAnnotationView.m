249ca274e9856a47738fae5f89cf711ba38d0480
Xin Wu <fiona330@126.com>
2015-04-22 15:54:05 -0400

    Update UserAnnotationView.m

--- WXWeibo/WXWeibo/UserAnnotationView.m
+++ WXWeibo/WXWeibo/UserAnnotationView.m
@@ -1,11 +1,3 @@
-//
-//  UserAnnotationView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-16.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "UserAnnotationView.h"
 #import "UserAnnotation.h"
 #import "UIImageView+WebCache.h"

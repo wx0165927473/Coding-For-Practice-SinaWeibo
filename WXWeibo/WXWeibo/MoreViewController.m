1e2e20df069064e48a50c820453f791689b3c61a
Xin Wu <fiona330@126.com>
2015-04-22 15:40:39 -0400

    Update MoreViewController.m

--- WXWeibo/WXWeibo/MoreViewController.m
+++ WXWeibo/WXWeibo/MoreViewController.m
@@ -1,11 +1,3 @@
-//
-//  MoreViewController.m
-//  WXWeibo
-//
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "MoreViewController.h"
 #import "ThemeViewController.h"
 #import "BrowModeController.h"

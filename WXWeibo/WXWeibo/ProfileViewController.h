50ffa2882634b30009dfc72468230e048e7fc5e9
Xin Wu <fiona330@126.com>
2015-04-22 15:43:27 -0400

    Update ProfileViewController.h

--- WXWeibo/WXWeibo/ProfileViewController.h
+++ WXWeibo/WXWeibo/ProfileViewController.h
@@ -1,12 +1,3 @@
-//
-//  ProfileViewController.h
-//  WXWeibo
-//  个人中心控制器
-
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 
 @interface ProfileViewController : BaseViewController

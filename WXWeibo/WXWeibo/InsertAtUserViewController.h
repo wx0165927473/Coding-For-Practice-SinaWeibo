8337d8b3b12597b53f1251eb0af5bd77fc441a9c
Xin Wu <fiona330@126.com>
2015-04-22 15:37:27 -0400

    Update InsertAtUserViewController.h

--- WXWeibo/WXWeibo/InsertAtUserViewController.h
+++ WXWeibo/WXWeibo/InsertAtUserViewController.h
@@ -1,10 +1,3 @@
-//
-//  InsertAtUserViewController.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-18.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
 typedef void (^SelectTopicBlock)(NSString *);
 
 #import "BaseViewController.h"

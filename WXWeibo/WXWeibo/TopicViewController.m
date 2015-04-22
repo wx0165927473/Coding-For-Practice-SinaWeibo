26372f146c1400d68647c2724d53e840dea12d1d
Xin Wu <fiona330@126.com>
2015-04-22 15:49:33 -0400

    Update TopicViewController.m

--- WXWeibo/WXWeibo/TopicViewController.m
+++ WXWeibo/WXWeibo/TopicViewController.m
@@ -1,11 +1,3 @@
-//
-//  TopicViewController.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-11.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "TopicViewController.h"
 #import "WeiboModel.h"
 

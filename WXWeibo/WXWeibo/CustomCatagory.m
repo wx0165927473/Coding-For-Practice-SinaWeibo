5605bf86cda626d6aa2a3f7aff0ed28e3c74747b
Xin Wu <fiona330@126.com>
2015-04-22 15:33:00 -0400

    Update CustomCatagory.m

--- WXWeibo/WXWeibo/CustomCatagory.m
+++ WXWeibo/WXWeibo/CustomCatagory.m
@@ -1,11 +1,3 @@
-//
-//  CustomCatagory.m
-//  WXMovie
-//
-//  Created by wei.chen on 12-7-2.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
-//
-
 #import "CustomCatagory.h"
 #import "ThemeManager.h"
 

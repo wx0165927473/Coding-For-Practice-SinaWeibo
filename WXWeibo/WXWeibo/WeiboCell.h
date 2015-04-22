1c840b31fd1b82cc29ebe68095b249ce6a861a1e
Xin Wu <fiona330@126.com>
2015-04-22 15:58:42 -0400

    Update WeiboCell.h

--- WXWeibo/WXWeibo/WeiboCell.h
+++ WXWeibo/WXWeibo/WeiboCell.h
@@ -1,11 +1,3 @@
-//
-//  WeiboCell.h
-//  WXWeibo
-//  自定义微博cell
-//  Created by wei.chen on 13-1-23.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @class WeiboModel;

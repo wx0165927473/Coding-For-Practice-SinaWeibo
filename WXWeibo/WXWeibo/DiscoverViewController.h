fe18b3df939ba23faa5d28ad916b147966d181d9
Xin Wu <fiona330@126.com>
2015-04-22 15:33:27 -0400

    Update DiscoverViewController.h

--- WXWeibo/WXWeibo/DiscoverViewController.h
+++ WXWeibo/WXWeibo/DiscoverViewController.h
@@ -1,12 +1,3 @@
-//
-//  DiscoverViewController.h
-//  WXWeibo
-//  广场控制器
-
-//  Created by wei.chen on 13-1-21.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "BaseViewController.h"
 
 @interface DiscoverViewController : BaseViewController

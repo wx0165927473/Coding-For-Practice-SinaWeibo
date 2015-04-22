33445098a6598a31507a42420870f6d9379da66f
Xin Wu <fiona330@126.com>
2015-04-22 15:57:21 -0400

    Update WXImageView.h

--- WXWeibo/WXWeibo/WXImageView.h
+++ WXWeibo/WXWeibo/WXImageView.h
@@ -1,11 +1,3 @@
-//
-//  WXImageView.h
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-10.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //点击头像 进入详情页面

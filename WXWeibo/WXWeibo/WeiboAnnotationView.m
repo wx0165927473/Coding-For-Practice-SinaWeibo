c72759bbe1207e936a9a935a3833950d46db4f2d
Xin Wu <fiona330@126.com>
2015-04-22 15:58:36 -0400

    Update WeiboAnnotationView.m

--- WXWeibo/WXWeibo/WeiboAnnotationView.m
+++ WXWeibo/WXWeibo/WeiboAnnotationView.m
@@ -1,11 +1,3 @@
-//
-//  WeiboAnnotationView.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-16.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "WeiboAnnotationView.h"
 #import "WeiboAnnotation.h"
 #import "WeiboModel.h"

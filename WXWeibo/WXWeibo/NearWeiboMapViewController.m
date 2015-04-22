4cdaa3e47942dd8bffbbbe275c860267eb4a00f7
Xin Wu <fiona330@126.com>
2015-04-22 15:42:21 -0400

    Update NearWeiboMapViewController.m

--- WXWeibo/WXWeibo/NearWeiboMapViewController.m
+++ WXWeibo/WXWeibo/NearWeiboMapViewController.m
@@ -1,11 +1,3 @@
-//
-//  NearWeiboMapViewController.m
-//  WXWeibo
-//
-//  Created by Wu Xin on 13-5-16.
-//  Copyright (c) 2013年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
-//
-
 #import "NearWeiboMapViewController.h"
 #import "WeiboModel.h"
 #import "WeiboAnnotation.h"
